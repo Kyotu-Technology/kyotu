30c79690b2bb4866c15b0918ecd49e222eed6796
Konrad Pagacz <konrad.pagacz@gmail.com>
2025-12-23 11:00:21 +0100

    java-simple-problem

---
+++ recruitment-challenges/small-java-question/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+  // Task
+  //
+  // Given a list of non-negative integers `nums`, count the number of unique pairs
+  // of indices (`i`, `j`) such that `nums[i] + nums[j]` is divisible by 3.
+  //
+  // Example 1
+  // [1,2,3,4,5]
+  // Answer: 3. Indices that fulfill criteria are: (1,2), (1,5), (2,4).
+  //
+  // Example 2
+  // [3,0,6,9]
+  // Answer: 6.
+  //
+  // Example 3
+  // [1]
+  // Answer: 0.
+  //
+  // Constraints:
+  // * Length of nums <= 10^5
+  // * nums[i] >= 0 for all i
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
