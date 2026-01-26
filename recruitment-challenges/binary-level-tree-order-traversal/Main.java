e363225754a7b9cfb6d002484f719f69e4313ab4
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-01-26 15:47:34 +0100

    added photo

--- recruitment-challenges/binary-level-tree-order-traversal/Main.java
+++ recruitment-challenges/binary-level-tree-order-traversal/Main.java
@@ -3,7 +3,6 @@
 // Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e.,
 // from left to right, level by level).
 //
-// Link to a jpeg: https://excalidraw.com/#json=RePXAmX3BNJ37xfu9a8qe,6-53r5vC7aqm9hf3Kcvbrg
 // Examples:
 // Input: root = [3,9,20,null,null,15,7]
 // Output: [[3],[9,20],[15,7]]
