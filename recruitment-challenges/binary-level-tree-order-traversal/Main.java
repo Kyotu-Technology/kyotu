c036ff29114efdb2932e06dba80953b0ce5c8142
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-01-26 15:46:12 +0100

    moved TreeNode out of Main

--- recruitment-challenges/binary-level-tree-order-traversal/Main.java
+++ recruitment-challenges/binary-level-tree-order-traversal/Main.java
@@ -2,6 +2,8 @@
 //
 // Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e.,
 // from left to right, level by level).
+//
+// Link to a jpeg: https://excalidraw.com/#json=RePXAmX3BNJ37xfu9a8qe,6-53r5vC7aqm9hf3Kcvbrg
 // Examples:
 // Input: root = [3,9,20,null,null,15,7]
 // Output: [[3],[9,20],[15,7]]
@@ -10,26 +12,6 @@
 // Output: [[1]]
 // Input: root = []
 // Output: []
-
-//  Definition for a binary tree node.
-public class TreeNode {
-  int val;
-  TreeNode left;
-  TreeNode right;
-
-  TreeNode() {}
-
-  TreeNode(int val) {
-    this.val = val;
-  }
-
-  TreeNode(int val, TreeNode left, TreeNode right) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
-}
-
 class Solution {
   public List<List<Integer>> levelOrder(TreeNode root) {}
 }
