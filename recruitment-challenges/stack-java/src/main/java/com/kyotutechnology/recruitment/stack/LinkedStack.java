b6266380c655e958ab4439b2af9dd65b404bfe5e
michalsikora <michal@devhelp.pl>
2026-04-08 09:53:13 +0200

    stack java

---
+++ recruitment-challenges/stack-java/src/main/java/com/kyotutechnology/recruitment/stack/LinkedStack.java
@@ -0,0 +1,37 @@
+package com.kyotutechnology.recruitment.stack;
+
+/**
+ * Linked-list-backed implementation of {@link Stack}.
+ */
+public class LinkedStack<E> implements Stack<E> {
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
---
+++ recruitment-challenges/stack-java/src/test/java/com/kyotutechnology/recruitment/stack/StackTest.java
@@ -0,0 +1,15 @@
+package com.kyotutechnology.recruitment.stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Stack} implementations.
+ */
+class StackTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+}
